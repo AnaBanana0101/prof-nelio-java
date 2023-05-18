779287cb80bd4f0d3ec57122c80c2be476a9925d
Ana Luiza Reis <120499627+AnaBanana0101@users.noreply.github.com>
2023-05-18 16:04:40 -0300

    Estudo sobre Sobrecarga

---
+++ sobrecarga/src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+/*
+ A sobrecarga é vocÊ disponibilizar mais de uma versão da mesma operação, tendo como diferença entre as operaçãoes
+ a lista de parametros, oq fica entre os parênteses; 
+ */
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+         
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        String name = scan.nextLine(); 
+        System.out.print("Price: ");
+        Double price = scan.nextDouble();
+        Product product = new Product(name, price);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = scan.nextInt();
+        product.addProduct(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+        System.out.println();
+
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = scan.nextInt(); 
+        product.removeProduct(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+
+    scan.close();
+    }
+    
+}
